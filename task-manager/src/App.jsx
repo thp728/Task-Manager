58206a29b85c0c0658feecf9efa10e00e0cce5af
thp728 <thp728@gmail.com>
2023-11-09 03:02:30 +0530

    Add task form

--- task-manager/src/App.jsx
+++ task-manager/src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 import { useState } from "react";
 
@@ -25,6 +26,16 @@ function App() {
     },
   ]);
 
+  // Add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = {
+      id,
+      ...task,
+    };
+    setTasks([...tasks, newTask]);
+  };
+
   // Delete task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
@@ -42,6 +53,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
